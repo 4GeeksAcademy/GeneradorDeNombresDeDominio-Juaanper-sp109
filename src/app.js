f4ccaeb425138f9db7ac4aa75657b2b7f7baec37
Juan Perez Rodriguez <juaanperez17@gmail.com>
2025-05-19 16:29:46 +0000

    First Commit

--- src/app.js
+++ src/app.js
@@ -7,5 +7,24 @@ import "./assets/img/4geeks.ico";
 
 window.onload = function() {
   //write your code here
+
+
+  let pronoun = ['the', 'our'];
+  let adj = ['great', 'big'];
+  let noun = ['jogger', 'racoon'];
+
+  for (let i = 0; i<pronoun.length; i++){
+    for (let i = 0; i<adj.length; i++){
+      for (let i = 0; i<adj.length; i++){
+        let (dominio) = pronoun [i] + adj [i] + noun[i] + .com
+        console.log (dominio);
+      }
+    }
+  }
+  console.log (dominio);
+
+
+
+
   console.log("Hello Rigo from the console!");
 };
